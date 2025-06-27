c04fcc76ea9f5b6606df28049b12a4a11612adce
ASWIN VIJAY <aswinviuk@gmail.com>
2025-06-28 00:59:58 +0530

    Refactor: Extract literals and magic numbers to Constants from Test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,47 +1,70 @@
 package com.gildedrose;
 
+import static com.gildedrose.constants.TestConstants.AGED_BRIE;
+import static com.gildedrose.constants.TestConstants.BACKSTAGE_PASSES;
+import static com.gildedrose.constants.TestConstants.COLUMN_HEADING;
+import static com.gildedrose.constants.TestConstants.DEXTERITY_VEST;
+import static com.gildedrose.constants.TestConstants.ELIXIR_OF_THE_MONGOOSE;
+import static com.gildedrose.constants.TestConstants.EXPIRED_BY_ONE_DAY;
+import static com.gildedrose.constants.TestConstants.FIFTEEN_DAYS_TO_SELL;
+import static com.gildedrose.constants.TestConstants.FIVE_DAYS_TO_SELL;
+import static com.gildedrose.constants.TestConstants.HEADING_PREFIX;
+import static com.gildedrose.constants.TestConstants.HEADING_SUFFIX;
+import static com.gildedrose.constants.TestConstants.ITEM_NAME_FOO;
+import static com.gildedrose.constants.TestConstants.LINE_BREAK;
+import static com.gildedrose.constants.TestConstants.MIN_QUALITY;
+import static com.gildedrose.constants.TestConstants.ONE_SHORT_OF_MAX_QUALITY;
+import static com.gildedrose.constants.TestConstants.QUALITY_OF_SEVEN;
+import static com.gildedrose.constants.TestConstants.QUALITY_OF_TWENTY;
+import static com.gildedrose.constants.TestConstants.SULFURAS;
+import static com.gildedrose.constants.TestConstants.SULFURAS_MAX_QUALITY;
+import static com.gildedrose.constants.TestConstants.TEN_DAYS_TO_SELL;
+import static com.gildedrose.constants.TestConstants.THIRTYONE_DAYS;
+import static com.gildedrose.constants.TestConstants.TWO_DAYS_TO_SELL;
+import static com.gildedrose.constants.TestConstants.ZERO_DAYS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
     @DisplayName("Item name should not change after update")
     void shouldReturnTheItemNameAsIs() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        Item[] items = new Item[]{new Item(ITEM_NAME_FOO, ZERO_DAYS, ZERO_DAYS)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals(ITEM_NAME_FOO, app.items[ZERO_DAYS].name);
     }
 
     @Test
     @DisplayName("should validate the legacy output for 31 days")
     void shouldMatchGoldenOutputOver31Days() {
-        Item[] items = new Item[]{new Item("+5 Dexterity Vest", 10, 20),
-                new Item("Aged Brie", 2, 0),
-                new Item("Elixir of the Mongoose", 5, 7),
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)};
+        Item[] items = new Item[]{new Item(DEXTERITY_VEST, TEN_DAYS_TO_SELL, QUALITY_OF_TWENTY),
+                new Item(AGED_BRIE, TWO_DAYS_TO_SELL, MIN_QUALITY),
+                new Item(ELIXIR_OF_THE_MONGOOSE, FIVE_DAYS_TO_SELL, QUALITY_OF_SEVEN),
+                new Item(SULFURAS, ZERO_DAYS, SULFURAS_MAX_QUALITY),
+                new Item(SULFURAS, EXPIRED_BY_ONE_DAY, SULFURAS_MAX_QUALITY),
+                new Item(BACKSTAGE_PASSES, FIFTEEN_DAYS_TO_SELL, QUALITY_OF_TWENTY),
+                new Item(BACKSTAGE_PASSES, TEN_DAYS_TO_SELL, ONE_SHORT_OF_MAX_QUALITY),
+                new Item(BACKSTAGE_PASSES, FIVE_DAYS_TO_SELL, ONE_SHORT_OF_MAX_QUALITY)};
 
         GildedRose app = new GildedRose(items);
         StringBuilder output = new StringBuilder();
 
-        for (int day = 0; day < 31; day++) {
-            output.append("-------- day ").append(day).append(" --------\n");
-            output.append("name, sellIn, quality").append("\n");
+        for (int day = ZERO_DAYS; day < THIRTYONE_DAYS; day++) {
+            output.append(HEADING_PREFIX).append(day).append(HEADING_SUFFIX);
+            output.append(COLUMN_HEADING).append(LINE_BREAK);
             for (Item item : items) {
-                output.append(item).append("\n");
+
+                output.append(item).append(LINE_BREAK);
             }
-            output.append("\n");
+            output.append(LINE_BREAK);
             app.updateQuality();
         }
-
         Approvals.verify(output.toString());
     }
 
---
+++ src/test/java/com/gildedrose/constants/TestConstants.java
@@ -0,0 +1,26 @@
+package com.gildedrose.constants;
+
+public class TestConstants {
+    public static final int ZERO_DAYS = 0;
+    public static final int EXPIRED_BY_ONE_DAY = -1;
+    public static final int TWO_DAYS_TO_SELL = 2;
+    public static final int FIVE_DAYS_TO_SELL = 5;
+    public static final int TEN_DAYS_TO_SELL = 10;
+    public static final int FIFTEEN_DAYS_TO_SELL = 15;
+    public static final int THIRTYONE_DAYS = 31;
+    public static final int MIN_QUALITY = 0;
+    public static final int QUALITY_OF_SEVEN = 7;
+    public static final int QUALITY_OF_TWENTY = 20;
+    public static final int ONE_SHORT_OF_MAX_QUALITY = 49;
+    public static final int SULFURAS_MAX_QUALITY = 80;
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String DEXTERITY_VEST = "+5 Dexterity Vest";
+    public static final String ITEM_NAME_FOO = "foo";
+    public static final String LINE_BREAK = "\n";
+    public static final String HEADING_PREFIX = "-------- day ";
+    public static final String HEADING_SUFFIX = " --------\n";
+    public static final String COLUMN_HEADING = "name, sellIn, quality";
+}
