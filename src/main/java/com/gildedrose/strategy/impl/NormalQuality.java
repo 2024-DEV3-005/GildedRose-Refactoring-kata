527a1d237f58ca1029e799373bce98c58379d755
ASWIN VIJAY <aswinviuk@gmail.com>
2025-06-28 17:12:28 +0530

    Refactor: Removed dead code from Normal Quality

--- src/main/java/com/gildedrose/strategy/impl/NormalQuality.java
+++ src/main/java/com/gildedrose/strategy/impl/NormalQuality.java
@@ -1,11 +1,6 @@
 package com.gildedrose.strategy.impl;
 
-import static com.gildedrose.constants.AppConstants.BACKSTAGE_PASSES;
 import static com.gildedrose.constants.AppConstants.BASE_QUALITY_UNIT;
-import static com.gildedrose.constants.AppConstants.MAX_QUALITY;
-import static com.gildedrose.constants.AppConstants.MIN_QUALITY;
-import static com.gildedrose.constants.AppConstants.QUALITY_OF_ELEVEN;
-import static com.gildedrose.constants.AppConstants.QUALITY_OF_SIX;
 
 import com.gildedrose.adapter.ItemAdapter;
 import com.gildedrose.strategy.ItemQuality;
@@ -13,25 +8,7 @@ import com.gildedrose.strategy.ItemQuality;
 public class NormalQuality implements ItemQuality {
 
     public void updateQualityBeforeSellInByDate(ItemAdapter item) {
-        if (!item.getName().equals(BACKSTAGE_PASSES)) {
-            if (item.getQuality() > MIN_QUALITY) {
-                item.decreaseQuality(BASE_QUALITY_UNIT);
-            }
-        } else {
-            if (item.getQuality() < MAX_QUALITY) {
-                item.increaseQuality(BASE_QUALITY_UNIT);
-
-                if (item.getName().equals(BACKSTAGE_PASSES)) {
-                    if (item.getSellIn() < QUALITY_OF_ELEVEN && item.getQuality() < MAX_QUALITY) {
-                        item.increaseQuality(BASE_QUALITY_UNIT);
-                    }
-
-                    if (item.getSellIn() < QUALITY_OF_SIX && item.getQuality() < MAX_QUALITY) {
-                        item.increaseQuality(BASE_QUALITY_UNIT);
-                    }
-                }
-            }
-        }
+        item.decreaseQuality(BASE_QUALITY_UNIT);
     }
 
     public void decrementSellInDays(ItemAdapter item) {
@@ -40,13 +17,7 @@ public class NormalQuality implements ItemQuality {
 
     public void updateQualityAfterSellInByDate(ItemAdapter item) {
         if (item.isItemExpired()) {
-            if (!item.getName().equals(BACKSTAGE_PASSES)) {
-                if (item.getQuality() > MIN_QUALITY) {
-                    item.decreaseQuality(BASE_QUALITY_UNIT);
-                }
-            } else {
-                item.setQuality(MIN_QUALITY);
-            }
+            item.decreaseQuality(BASE_QUALITY_UNIT);
         }
     }
 }
--- src/test/java/com/gildedrose/strategy/impl/NormalQualityTest.java
+++ src/test/java/com/gildedrose/strategy/impl/NormalQualityTest.java
@@ -1,7 +1,62 @@
 package com.gildedrose.strategy.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.gildedrose.constants.TestConstants.EXPIRED_BY_ONE_DAY;
+import static com.gildedrose.constants.TestConstants.FIVE_DAYS_TO_SELL;
+import static com.gildedrose.constants.TestConstants.FOUR_DAYS_TO_SELL;
+import static com.gildedrose.constants.TestConstants.ITEM_NAME_FOO;
+import static com.gildedrose.constants.TestConstants.QUALITY_OF_EIGHTEEN;
+import static com.gildedrose.constants.TestConstants.QUALITY_OF_SIXTEEN;
+import static com.gildedrose.constants.TestConstants.QUALITY_OF_TWENTY;
+import static com.gildedrose.constants.TestConstants.QUALITY_OF_TWENTY_ONE;
+import static com.gildedrose.constants.TestConstants.TEN_DAYS_TO_SELL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.gildedrose.Item;
+import com.gildedrose.adapter.ItemAdapter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class NormalQualityTest {
 
+    NormalQuality normalQuality;
+
+    @BeforeEach
+    void setUp() {
+        normalQuality = new NormalQuality();
+    }
+
+    @Test
+    @DisplayName("Normal item quality should degrade by 1 before sell-in date")
+    void shouldDecreaseQualityByOneIfNotExpired() {
+        Item normalItem = new Item(ITEM_NAME_FOO, TEN_DAYS_TO_SELL, QUALITY_OF_TWENTY_ONE);
+        ItemAdapter itemAdapter = new ItemAdapter(normalItem);
+
+        normalQuality.updateQualityBeforeSellInByDate(itemAdapter);
+
+        assertEquals(QUALITY_OF_TWENTY, itemAdapter.getQuality(), "Normal item quality should decrease by 1");
+    }
+
+    @Test
+    @DisplayName("SellIn days should reduce by 1 for normal item")
+    void shouldDecreaseSellInDaysByOneForNormal() {
+        Item normalItem = new Item(ITEM_NAME_FOO, FIVE_DAYS_TO_SELL, QUALITY_OF_TWENTY);
+        ItemAdapter itemAdapter = new ItemAdapter(normalItem);
+
+        normalQuality.decrementSellInDays(itemAdapter);
+
+        assertEquals(FOUR_DAYS_TO_SELL, itemAdapter.getSellIn(), "SellIn value should reduce by 1");
+    }
+
+    @Test
+    @DisplayName("Normal item quality should degrade by 2 after sell-in date")
+    void shouldDecreaseQualityByOneIfExpired() {
+        Item normalItem = new Item(ITEM_NAME_FOO, EXPIRED_BY_ONE_DAY, QUALITY_OF_EIGHTEEN);
+        ItemAdapter itemAdapter = new ItemAdapter(normalItem);
+
+        normalQuality.updateQualityBeforeSellInByDate(itemAdapter);
+        normalQuality.updateQualityAfterSellInByDate(itemAdapter);
+
+        assertEquals(QUALITY_OF_SIXTEEN, itemAdapter.getQuality(), "Normal item quality should decrease by 2 after sell-in date");
+    }
 }
